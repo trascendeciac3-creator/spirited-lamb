5457aeadd072119e82f0da06d48935384b97e379
trascendeciac3-creator <trascendeciac3@gmail.com>
2026-02-24 15:00:09 -0800

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,1 +1,27 @@
-Permission denied
+export type County = 'Ventura' | 'Santa Barbara';
+export type EventType = 'Spiritual' | 'Action' | 'Connection';
+
+export interface Event {
+  id: string;
+  title: string;
+  description: string;
+  start: Date;
+  end: Date;
+  parishName: string;
+  address: string;
+  city: string;
+  county: County;
+  lat: number;
+  lng: number;
+  tags: string[];
+  type: EventType;
+  imageUrl?: string;
+}
+
+export interface ShopItem {
+  id: string;
+  name: string;
+  price: string;
+  imageUrl: string;
+  category: 'Apparel' | 'Home' | 'Accessories';
+}
